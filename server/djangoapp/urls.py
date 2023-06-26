840646dec4217157a622408d4fde1891298ec3de
Anaidasiv <sreekunjus555@gmail.com>
2023-06-26 05:12:19 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
+from djangoapp import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for about view
+    path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
+     path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
 
