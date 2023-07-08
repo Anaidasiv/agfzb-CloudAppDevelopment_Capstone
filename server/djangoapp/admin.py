e2588412bf3a493b101819b14f7a4a9223677f66
Anaidasiv <sreekunjus555@gmail.com>
2023-07-08 18:49:35 -0400

    changes after carmake

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
-
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+    inlines = [CarModelInline]
+
+# CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['car_make', 'name', 'dealer_id', 'types', 'year']
+    list_filter = ['types', 'car_make', 'dealer_id', 'year']
+    search_fields = ['car_make', 'name']
 
 # Register models here
+if not admin.site.is_registered(CarMake):
+    admin.site.register(CarMake, CarMakeAdmin)
+
+if not admin.site.is_registered(CarModel):
+    admin.site.register(CarModel)
+
